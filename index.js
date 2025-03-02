f326786be1c54bd7a642e8d511458305d9a7b2cc
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-03-02 16:26:38 +0530

    feature: Add code to read data from the CSV file

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 const db = require('./db');
+const fs = require('fs');
+const { parse } = require('csv-parse');
+
+const stockData = [];
+
+fs.createReadStream('./data-source/Quote-Equity-TCS-EQ-01-01-2024-to-31-12-2024.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+        bom: true,  // Add this line to handle UTF-8 BOM
+        trim: true, // Add this to trim whitespace from fields
+        skip_empty_lines: true,
+        relax_quotes: true // More lenient quote handling
+    }))
+    .on('data', (data) => {
+        console.log('data', data);
+        stockData.push(data);
+    })
+    .on('error', (error) => {
+        console.log('Error while reading the file!');
+        console.log('Error:', error);
+    })
+    .on('end', () => {
+        console.log('Completed reading the file');
+        console.log(`${stockData.length} records found!`);
+    });
 
 // Basic database connection test
 async function testDatabaseConnection()
